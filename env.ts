553fb2daa3a871cbde5520326ed3408eb501c422
Kamil <kamil@oczkow.ski>
2025-07-28 20:02:12 +0100

    prep for auth

--- env.ts
+++ env.ts
@@ -7,9 +7,14 @@ loadEnvConfig(projectDir)
  
 export const env = createEnv({
   server: {
+    // Postgres
     POSTGRES_PRISMA_URL: z.string().min(1),
+    // Google OAuth
+    CLIENT_ID: z.string().min(1),
+    CLIENT_SECRET: z.string().min(1),
   },
   client: {
+    // Project specific
     NEXT_PUBLIC_BRAND_NAME: z.string().min(1),
     NEXT_PUBLIC_COMPANY_NAME: z.string().min(1),
   },
@@ -17,5 +22,7 @@ export const env = createEnv({
     NEXT_PUBLIC_BRAND_NAME: process.env.NEXT_PUBLIC_BRAND_NAME,
     NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME,
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
   },
 });
