c6d828eea2dd87deda97c999086ec79208da1584
Kamil Oczkowski <kamillozz7k@gmail.com>
2025-07-31 19:24:45 +0100

    use next image instance of img

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -1,21 +1,27 @@
 import { auth0 } from "@/lib/auth0";
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 
 const BusinessDashboard = async () => {
     const session = await auth0.getSession();
 
     if (!session) {
-        return <div>Not authenticated. <Link href="/auth/login">Log in</Link></div>;
+        return (
+            <div>
+                Not authenticated. <Link href="/auth/login">Log in</Link>
+            </div>
+        );
     }
     return (
         <>
             <p>Dashboard</p>
             <div>
-                <img src={session?.user.picture} alt={session?.user.name} />{" "}
-                <h2>{session?.user.name}</h2> <p>{session?.user.email}</p>{" "}
+                {session.user.picture && session.user.name && (
+                    <Image src={session?.user.picture} alt={session?.user.name} />
+                )}
+                <h2>{session?.user.name}</h2> <p>{session?.user.email}</p>
                 <Link href="/auth/logout">Log out</Link>
             </div>
-
         </>
     );
 };
