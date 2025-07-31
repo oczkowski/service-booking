b7f33d414c835ef4a2be0925a6f8df2edf259edb
Kamil Oczkowski <kamillozz7k@gmail.com>
2025-07-31 20:09:48 +0100

    Fix env var bs

--- env.ts
+++ env.ts
@@ -20,7 +20,7 @@ export const env = createEnv({
     AUTH0_CLIENT_SECRET: z.string().min(1),
     // AUTH0_AUDIENCE: z.string().min(1),
     // AUTH0_SCOPE: z.string().min(1),
-    AUTH0_HOOK_SECRET: z.string().min(10)
+    AUTH0_HOOK_SECRET: z.string().min(10),
   },
   client: {
     // Project specific
@@ -37,13 +37,15 @@ export const env = createEnv({
     // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     // Auth0
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_BASE_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : process.env.AUTH0_BASE_URL,
     AUTH0_SECRET: process.env.AUTH0_SECRET,
     AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     // AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     // AUTH0_SCOPE: process.env.AUTH0_SCOPE,
-    AUTH0_HOOK_SECRET: process.env.AUTH0_HOOK_SECRET
+    AUTH0_HOOK_SECRET: process.env.AUTH0_HOOK_SECRET,
   },
 });
